c9e4362fa99764e14c2e3dcc26a704bdb1d8984e
Dain M. Blodorn Kim <dain@db13.us>
2025-01-16 18:48:41 -0800

    use puppeteer

--- src/lib/getGFMCampaignData.ts
+++ src/lib/getGFMCampaignData.ts
@@ -1,67 +1,76 @@
-import phantom from "phantom";
+import puppeteer from "puppeteer-core";
 import * as cheerio from "cheerio";
 import { CAMPAIGNS } from "../consts/campaigns";
 
 export type CampaignData = {
   campaignName: string;
-  status: string;
   title: string;
   currentAmount: number;
   goalAmount: number;
   percentage: number;
   fundName: string;
   photo: string;
-};
+} | null;
 
 export async function getGFMCampaignData(campaignName: string) {
-  const instance = await phantom.create();
-  const page = await instance.createPage();
-  
-  await page.on("onResourceRequested", function (requestData) {
-    console.info("Requesting", requestData.url);
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.BLESS_TOKEN}`,
   });
+  
 
-  const status = await page.open(`https://www.gofundme.com/f/${campaignName}`);
-  const content = await page.property("content");
+  try {
+    const page = await browser.newPage();
 
-  await instance.exit();
+    await page.goto(`https://www.gofundme.com/f/${campaignName}`);
+    const content = await page.content();
+    
+    const $ = cheerio.load(content);
+    const title = $("title").text();
+    const nextData = $("#__NEXT_DATA__").html();
 
-  const $ = cheerio.load(content);
-  const title = $("title").text();
-  const nextData = $("#__NEXT_DATA__").html();
+    const parsedData =
+      nextData && JSON.parse(nextData)["props"]["pageProps"]["__APOLLO_STATE__"];
 
-  const parsedData =
-    nextData && JSON.parse(nextData)["props"]["pageProps"]["__APOLLO_STATE__"];
+    const fundraiserData = Object.keys(parsedData).reduce((acc, key) => {
+      if (key.startsWith("Fundraiser")) {
+        // @ts-expect-error: we don't have the types
+        acc[key] = parsedData[key];
+      }
+      return acc;
+    }, {});
 
-  const fundraiserData = Object.keys(parsedData).reduce((acc, key) => {
-    if (key.startsWith("Fundraiser")) {
-      // @ts-expect-error: we don't have the types
-      acc[key] = parsedData[key];
+    // Fundraiser Data
+    const firstFundraiserKey = Object.keys(fundraiserData)[0];
+    const parsedFundraiserData =
+      fundraiserData[firstFundraiserKey as keyof typeof fundraiserData];
+
+    const currentAmount = parsedFundraiserData["currentAmount"]["amount"];
+    const goalAmount = parsedFundraiserData["goalAmount"]["amount"];
+    const percentage = Number(((currentAmount / goalAmount) * 100).toFixed(2));
+    const photo = parsedFundraiserData["fundraiserImageUrl"];
+    const fundName = parsedFundraiserData["fundName"];
+
+    return {
+      campaignName,
+      title,
+      currentAmount,
+      goalAmount,
+      percentage,
+      fundName,
+      photo,
+    };
+  } catch {
+    // console.error(e);
+    return {
+      campaignName,
+      title: "",
+      currentAmount: 0,
+      goalAmount: 0,
+      percentage: 0,
+      fundName: "",
+      photo: "",
     }
-    return acc;
-  }, {});
-
-  // Fundraiser Data
-  const firstFundraiserKey = Object.keys(fundraiserData)[0];
-  const parsedFundraiserData =
-    fundraiserData[firstFundraiserKey as keyof typeof fundraiserData];
-
-  const currentAmount = parsedFundraiserData["currentAmount"]["amount"];
-  const goalAmount = parsedFundraiserData["goalAmount"]["amount"];
-  const percentage = Number(((currentAmount / goalAmount) * 100).toFixed(2));
-  const photo = parsedFundraiserData["fundraiserImageUrl"];
-  const fundName = parsedFundraiserData["fundName"];
-
-  return {
-    campaignName,
-    status,
-    title,
-    currentAmount,
-    goalAmount,
-    percentage,
-    fundName,
-    photo,
-  };
+  }
 }
 
 export async function getAllCampaigns() {
@@ -73,5 +82,5 @@ export async function getAllCampaigns() {
     (data) => data
   );
 
-  return campaignData;
+  return campaignData.filter((campaign) => !!campaign);
 }
--- src/pages/api/all.ts
+++ src/pages/api/all.ts
@@ -1,16 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getGFMCampaignData, CampaignData } from "../../lib/getGFMCampaignData";
-
-import { CAMPAIGNS } from "../../consts/campaigns";
+import { getAllCampaigns, CampaignData } from "../../lib/getGFMCampaignData";
 
 export default async function handler(_: NextApiRequest, res: NextApiResponse<CampaignData[]>) {
-  const campaignDataPromises = CAMPAIGNS.map((campaign) =>
-    getGFMCampaignData(campaign)
-  );
-
-  const campaignData = await Promise.all(campaignDataPromises).then(
-    (data) => data
-  );
+  const campaignData = await getAllCampaigns();
   
   res.status(200).json(campaignData);
 }
