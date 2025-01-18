5b9f3d06bb3d13549337dfecbc5913a481d60e59
Dain M. Blodorn Kim <dain@db13.us>
2025-01-17 16:22:26 -0800

    spruce up index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,14 +19,11 @@ export default function Home({
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className="grid grid-cols-1 gap-2 p-4">
-      <h1>Aveson Go Fund Me Campagins:</h1>
-      <Link className="underline" href="/api/all">
-        Data
-      </Link>
-      <ul className="grid grid-cols-4 gap-4">
+      <h1>Aveson Community Go Fund Me Campagins:</h1>
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
         {campaigns.map((campaign) => (
           <li
-            className="container p-2 border border-1 border-inherit"
+            className="container p-3 border border-1 border-inherit"
             key={campaign.title}
           >
             <img
@@ -34,13 +31,25 @@ export default function Home({
               src={campaign.photo}
               alt={campaign.title}
             />
-            <div>
-              <h2>{campaign.title}</h2>
-              <p>
-                {campaign.fundingData.currentAmount.formatted} /{" "}
-                {campaign.fundingData.goalAmount.formatted}
-              </p>
-              <p>{campaign.fundingData.percentage}%</p>
+            <div className="flex flex-row justify-between pt-3">
+              <div>
+                <h2>{campaign.title}</h2>
+                <p>
+                  {campaign.fundingData.currentAmount.formatted} /{" "}
+                  {campaign.fundingData.goalAmount.formatted}
+                </p>
+                <p>{campaign.fundingData.percentage}%</p>
+              </div>
+              <div className="flex flex-col">
+                <Link
+                  href={campaign.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                >
+                  Support
+                </Link>
+              </div>
             </div>
           </li>
         ))}
